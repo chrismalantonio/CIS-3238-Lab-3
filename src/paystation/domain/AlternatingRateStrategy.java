51b91c18f3ecd0a86a36e92003947ebb14d2b16a
cmala <cmala@192.168.1.120>
2017-09-20 23:59:59 -0400

    implemented AlternatingRateStrategy

--- src/paystation/domain/AlternatingRateStrategy.java
+++ src/paystation/domain/AlternatingRateStrategy.java
@@ -5,13 +5,36 @@
  */
 package paystation.domain;
 
+import java.util.Date;
+import java.util.Calendar; 
+import java.util.GregorianCalendar;
+
 /**
  * Alternating Rate Strategy for gammatown
- * Rate strategy varies between Linear rate strategy on weekdays 
- * and Progressive rate stategy on weekends
+ * Rate strategy varies between Linear rate strategy on weekends 
+ * and Progressive rate stategy on weekdays
  */
 public class AlternatingRateStrategy implements RateStrategy {
+    RateStrategy weekendStrategy, weekdayStrategy, currentState;
+    public AlternatingRateStrategy(RateStrategy weekdayStrategy, RateStrategy weekendStrategy){
+        this.weekdayStrategy = weekdayStrategy;
+        this.weekendStrategy = weekendStrategy; 
+        this.currentState = null; 
+    }
     public int calculateTime(int amount){
-        return amount * 2 / 5;
+        if (isWeekend()){
+            currentState = weekendStrategy;
+        } else {
+            currentState = weekdayStrategy; 
+        }
+        return currentState.calculateTime(amount);
+    }
+    
+    private boolean isWeekend(){
+        Date d = new Date();
+        Calendar c = new GregorianCalendar();
+        c.setTime(d);
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        return (dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY);
     }
 }
